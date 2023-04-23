29b54ccde01b214b77aa87398f5be9886f0047d8
dmratcliffe <git@rmnived.com>
2023-04-23 03:57:41 +0000

    init

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<app-grid>
+  <toolbar />
+  <columns />
+</app-grid>
+
+<style>
+  app-grid {
+    display: grid;
+    grid-template-rows: 50px 1fr;
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      'toolbar'
+      'columns';
+  }
+  toolbar {
+    align-items: end;
+    justify-content: center;
+    grid-area: toolbar;
+  }
+  columns {
+    grid-area: columns;
+  }
+</style>
